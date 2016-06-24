a51343ee753777c79cad44fb1f95e4baa1daa2f9
Hyojin Jung <hyojinjenny@gmail.com>
2016-06-24 23:48:30 +1200

    Showing posts on the stream page

--- app/controllers/StreamController.php
+++ app/controllers/StreamController.php
@@ -7,6 +7,10 @@ class StreamController extends PageController {
 
 	// Constructor
 	public function __construct($dbc) {
+
+		// Run the parent constructor
+		parent::__construct();
+
 		$this->dbc = $dbc;
 
 		// If you are not logged in
@@ -19,5 +23,32 @@ class StreamController extends PageController {
 	// Methods (functions)
 	public function buildHTML() {
 
+		// Get latest posts (pins)
+		$allData = $this->getLatestPosts();
+
+		// Prepare an empty data array
+		$data = [];
+
+		$data['allPosts'] = $allData;
+
+		// Send stream and data to the actual page
+		echo $this->plates->render('stream', $data);
+
+	}
+
+	private function getLatestPosts() {
+
+		// Prepare some SQL
+		$sql = "SELECT *
+				FROM posts";
+
+		// Run the SQL and capture the result
+		$result = $this->dbc->query($sql);
+
+		// Extract the result as an array
+		$allData = $result->fetch_all(MYSQLI_ASSOC);
+
+		// Return the results to the code that called this function
+		return $allData;
 	}
 }
