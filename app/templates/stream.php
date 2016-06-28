98c7713b55ea6debeb4eb21d5fce27db0d7c8bc1
Hyojin Jung <hyojinjenny@gmail.com>
2016-06-29 11:28:58 +1200

    Make a relationship between posts and users

--- app/templates/stream.php
+++ app/templates/stream.php
@@ -42,12 +42,14 @@
       <article class="grid-item">
 
       <a href="index.php?page=post&postid=<?= $item['id'] ?>">
-        <img src="<?= $item['image'] ?>" alt="">
+        <img src="<?= $item['image'] ?>" alt="" height="500" >
       </a>
         <div class="grid-content">
           <a href="" class="button alert save"><i class="fa fa-thumb-tack" aria-hidden="true"></i> Save</a>
           <a href="" class="button secondary heart"><i class="fa fa-heart" aria-hidden="true"></i></a>
-          <h1><?= $item['title'] ?></h1>
+          <h1>
+            <a href="index.php?page=post&postid=<?= $item['id'] ?>"><?= $item['title'] ?></a>
+          </h1>
           <a href=""><i class="fa fa-thumb-tack" aria-hidden="true"></i> 15</a>
           <a href=""><i class="fa fa-heart" aria-hidden="true"></i> 3</a>
           <div class="media-object">
